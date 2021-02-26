add8ca5ff99d4001ca1f5d90e8b28ef8f48d1b18
AnnKemp <ann.kemp@scarlet.be>
2021-02-26 21:22:19 +0100

    end

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var dt=require('./datetimemodule');
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: "+ dt.myDateTime())
+    res.write("The date and time are currently: "+ dt.myDateTime());
     res.end('<br>Hello World!');
 }).listen(8080);
 
